2783d70d7e85350723b76979a44bc6b198826200
ccol168 <colettaclaudio11@gmail.com>
2024-02-13 12:58:09 +0100

    Made RunGenerator more flexible for CfgFiles inputs

--- RunGenerator.py
+++ RunGenerator.py
@@ -72,74 +72,60 @@ if not os.path.exists("Events/" + NameRun + '/configs'):
 if not os.path.exists("Events/" + NameRun + '/analyzed'):
     os.makedirs("Events/" + NameRun + '/analyzed')
 
+DirPath = os.getcwd() + "/Events/" + NameRun + "/"
+
 #Reading the configuration file and copying it for each run
 for i in range(0,Runs) :
     ReadCfgFile = open(CfgFile,'r')
-    WriteFile = open("Events/" + NameRun + "/configs/CfgFile_" +str(i).zfill(4)+".txt","w")
+    WriteFileCfg = open("Events/" + NameRun + "/configs/CfgFile_" +str(i).zfill(4)+".txt","w")
 
-    for j in range (0,2) :
-        line = ReadCfgFile.readline()
-        WriteFile.write(line)
+    for line in ReadCfgFile:
+        line_content = line.split()
 
-    ReadCfgFile.readline()
-    WriteFile.write("NEvents " + str(EventsPerRun) + "\n")
-
-    for j in range(0,13) :
-        line = ReadCfgFile.readline()
-        WriteFile.write(line)
-
-    #reading if it is backgrounds or not    
-    line = ReadCfgFile.readline()
-    WriteFile.write(line)
-    content = line.split()
-    IsBackgrounds = content[1]
-
-    #reading the last cut
-    line = ReadCfgFile.readline()
-    WriteFile.write(line)
+        if line_content[0] == "NEvents" :
+            output_line = "NEvents " + str(EventsPerRun) + "\n"
+        else :
+            output_line = line
+            if line_content[0] == "IsBackgrounds" :
+                IsBackgrounds = line_content[1]
+        WriteFileCfg.write(output_line)
 
     ReadCfgFile.close()
-    WriteFile.close()
-
-DirPath = os.getcwd() + "/Events/" + NameRun + "/"
+    WriteFileCfg.close()
 
 #writing the .sh files
-for i in range(0,Runs) :
-    WriteFile = open("Events/" + NameRun + "/sh/Script_" +str(i).zfill(4)+".sh","w")
-    WriteFile.write('#!/bin/bash \n')
-    WriteFile.write('source /cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc1120/Pre-Release/J23.1.0-rc0/setup.sh \n \n')
+    WriteFileSh = open("Events/" + NameRun + "/sh/Script_" +str(i).zfill(4)+".sh","w")
+    WriteFileSh.write('#!/bin/bash \n')
+    WriteFileSh.write('source /cvmfs/juno.ihep.ac.cn/centos7_amd64_gcc1120/Pre-Release/J23.1.0-rc0/setup.sh \n \n')
 
-    WriteFile.write(os.getcwd() + '/Directionality_ToyMC  ')
-    WriteFile.write(DirPath + 'configs/CfgFile_' + str(i).zfill(4) + '.txt  ' )
-    WriteFile.write(DirPath + 'root/' + NameRun + '_' + str(i).zfill(4) + '.root \n \n')
+    WriteFileSh.write(os.getcwd() + '/Directionality_ToyMC  ')
+    WriteFileSh.write(DirPath + 'configs/CfgFile_' + str(i).zfill(4) + '.txt  ' )
+    WriteFileSh.write(DirPath + 'root/' + NameRun + '_' + str(i).zfill(4) + '.root \n \n')
 
-    WriteFile.write('sleep 10 \n \n')
+    WriteFileSh.write('sleep 10 \n \n')
 
-    WriteFile.write(os.getcwd() + '/Ordered_hits  ')
-    WriteFile.write(DirPath + 'root/' + NameRun + '_' + str(i).zfill(4) + '.root  ')
-    WriteFile.write(DirPath + 'analyzed/Analyzed_' + NameRun + '_' + str(i).zfill(4) + '.root  60 ' + IsBackgrounds)
+    WriteFileSh.write(os.getcwd() + '/Ordered_hits  ')
+    WriteFileSh.write(DirPath + 'root/' + NameRun + '_' + str(i).zfill(4) + '.root  ')
+    WriteFileSh.write(DirPath + 'analyzed/Analyzed_' + NameRun + '_' + str(i).zfill(4) + '.root  60 ' + IsBackgrounds)
 
-    WriteFile.close()
+    WriteFileSh.close()
 
 #writing the .sub files 
 
-for i in range(0,Runs) :
-    WriteFile = open("Events/" + NameRun + "/sub/Script_" +str(i).zfill(4)+".sub","w")
-    WriteFile.write('universe = vanilla\n')
-    WriteFile.write('getenv = true\n')
-    WriteFile.write('executable = ' + os.getcwd() + "/Events/" + NameRun + "/sh/Script_" + str(i).zfill(4) + '.sh \n')
-    WriteFile.write('log = ' + os.getcwd() + "/Events/" + NameRun + '/log/' + NameRun + '_' + str(i).zfill(4) + '.log\n')
-    WriteFile.write('output = ' + os.getcwd() + "/Events/" + NameRun + '/out/' + NameRun + '_' + str(i).zfill(4)  + '.out\n')
-    WriteFile.write('error = ' + os.getcwd() + "/Events/" + NameRun + '/err/' + NameRun + '_' + str(i).zfill(4) + '.err\n')
-    WriteFile.write('+MaxRuntime = 86400\n')
-    WriteFile.write('ShouldTransferFiles = YES\n')
-    WriteFile.write('WhenToTransferOutput = ON_EXIT\n')
-    WriteFile.write('queue 1\n')
-    WriteFile.close()
+    WriteFileSub = open("Events/" + NameRun + "/sub/Script_" +str(i).zfill(4)+".sub","w")
+    WriteFileSub.write('universe = vanilla\n')
+    WriteFileSub.write('getenv = true\n')
+    WriteFileSub.write('executable = ' + os.getcwd() + "/Events/" + NameRun + "/sh/Script_" + str(i).zfill(4) + '.sh \n')
+    WriteFileSub.write('log = ' + os.getcwd() + "/Events/" + NameRun + '/log/' + NameRun + '_' + str(i).zfill(4) + '.log\n')
+    WriteFileSub.write('output = ' + os.getcwd() + "/Events/" + NameRun + '/out/' + NameRun + '_' + str(i).zfill(4)  + '.out\n')
+    WriteFileSub.write('error = ' + os.getcwd() + "/Events/" + NameRun + '/err/' + NameRun + '_' + str(i).zfill(4) + '.err\n')
+    WriteFileSub.write('+MaxRuntime = 86400\n')
+    WriteFileSub.write('ShouldTransferFiles = YES\n')
+    WriteFileSub.write('WhenToTransferOutput = ON_EXIT\n')
+    WriteFileSub.write('queue 1\n')
+    WriteFileSub.close()
 
 #writing the CondorScriptToLaunch.sh 
-
-
 WriteFile = open("Events/" + NameRun + '/CondorScriptToLaunch_' + NameRun + '.sh','w') 
 
 for i in range(0,Runs) :
